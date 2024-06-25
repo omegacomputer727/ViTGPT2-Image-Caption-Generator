4decd2a49ec8d6cfec10d5664335d84e7bac4eb0
animesh <116508236+omegacomputer727@users.noreply.github.com>
2024-06-25 18:52:52 +0530

    Add files via upload

---
+++ training/modules.py
@@ -0,0 +1,38 @@
+import numpy as np
+import nltk
+import evaluate
+
+metric = evaluate.load("rouge")
+
+def postprocess_text(preds, labels):
+    preds = [pred.strip() for pred in preds]
+    labels = [label.strip() for label in labels]
+
+    preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
+    labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
+
+    return preds, labels
+
+ignore_pad_token_for_loss = True
+
+def compute_metrics(eval_preds, tokenizer, metric = metric):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    if ignore_pad_token_for_loss:
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    decoded_preds, decoded_labels = postprocess_text(decoded_preds,
+                                                     decoded_labels)
+
+    result = metric.compute(predictions=decoded_preds,
+                            references=decoded_labels,
+                            use_stemmer=True)
+    result = {k: round(v * 100, 4) for k, v in result.items()}
+    prediction_lens = [
+        np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds
+    ]
+    result["gen_len"] = np.mean(prediction_lens)
+    return result
